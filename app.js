9bafa23f06161e4d53143d1f6afbaabf0f9c3d1a
niseguy <dmy4874@rit.edu>
2018-06-06 14:31:40 -0400

    Sass addition

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app = express()
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function (req, res)
 {
     res.render('index');
 });
+
+
 
 app.get('/projects', function (req, res)
 {
