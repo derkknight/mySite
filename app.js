10a93ada526cb5bce72f7036edf99cfbb61811d2
niseguy <dmy4874@rit.edu>
2018-06-05 20:34:46 -0400

    Skeletoned out the website.

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res)
+{
+    res.render('index');
+});
+
+app.get('/projects', function (req, res)
+{
+    res.render('projects');    
+});
+
+app.get('/github', function (req, res)
+{
+    res.send('Projects page');
+});
+
+app.get('/resume', function (req, res)
+{
+    res.send('Resume page');    
+});
 
 app.listen(3000, () => console.log('Listening on Port 3000'))
