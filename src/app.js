590d00c67be5ba17ab6e0ba42856633db0d96dd2
niseguy <dmy4874@rit.edu>
2018-07-01 22:46:46 -0400

    Webpack overhaul

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
+import 'bootstrap';
+import _ from 'lodash';
+
 const express = require('express')
 const app = express()
+
+function component() {
+    var element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+    return element;
+}
+
+document.body.appendChild(component());
 
 app.set('view engine', 'ejs');
 
